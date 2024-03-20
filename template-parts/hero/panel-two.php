0b450d7c8a7d8fcf7a87851137316c6d02c19de5
Andy Paddock <appaddock@gmail.com>
2024-03-20 11:39:23 +0000

    panel two

--- template-parts/hero/panel-two.php
+++ template-parts/hero/panel-two.php
@@ -2,20 +2,21 @@
     <?php $panelTwoAlt = get_sub_field('panel_two_alt'); ?>
     <div class="panel-two--text">
         <div class="map-elements alternate">
-            <p><?php echo $panelTwoAlt['starter_text']; ?>
 
-                <?php if( have_rows('panel_two_alt') ):?>
-                <?php while ( have_rows('panel_two_alt') ) : the_row(); 
+
+            <?php if( have_rows('panel_two_alt') ):?>
+            <?php while ( have_rows('panel_two_alt') ) : the_row(); 
 
     if( have_rows('map_element_alt') ): while ( have_rows('map_element_alt') ) : the_row();?>
-
+            <p>
+                <?php the_sub_field('pre_text');?>
                 <span id="block-<?php echo get_row_index(); ?>"><?php the_sub_field('title');?></span>
                 <?php the_sub_field('description');?>
-
-                <?php endwhile; endif;
+            </p>
+            <?php endwhile; endif;
 
 endwhile;?> <?php endif;?>
-            </p>
+
         </div>
     </div>
     <div class="panel-two--filler">
